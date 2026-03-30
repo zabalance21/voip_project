b61ee58d7d21fa515fb87514dbe18853146325ae
zabalance21 <ronin_zerna@dlsu.edu.ph>
2026-03-30 18:54:17 +0800

    added sdp creation and extraction of rtp from clients

--- src/sip/SdpBuilder.java
+++ src/sip/SdpBuilder.java
@@ -1,5 +1,18 @@
 package sip;
 
+// SDP body (minimal)
 public class SdpBuilder {
-    public static String build()
+    public static String build(String localIp, int rtpPort){
+        return "v=0\r\n" + "o=- 0 0 IN IP4 " + localIp + "\r\n" + "s=VoIP Session\r\n" + "t=0 0\r\n" +
+                "m=audio " + rtpPort + " RTP/AVP 0 \r\n" + "a=rtpmap:0 PCMU/8000\r\n";
+    }
+
+    public static int extractRtpPort(String sdp) {
+        for (String line : sdp.split("\r\n")) {
+            if (line.startsWith("m=audio")) {
+                return Integer.parseInt(line.split(" ")[1]);
+            }
+        }
+        return -1;
+    }
 }
